65ccfda29f979c8b01859f463a78b3ec583aaeab
nismal <110675248+nismalrfd@users.noreply.github.com>
2023-06-10 12:50:15 +0530

    Add files via upload

--- sellover/settings.py
+++ sellover/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gwa49w&nb42fyui3$rk#gx6!)73979ux)(9kzljptp$&umdw6g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -119,10 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL ='media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
+STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+STATICFILES_DIRS =os.path.join(BASE_DIR, 'static'),
+MEDIA_ROOT = BASE_DIR / 'static/media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
