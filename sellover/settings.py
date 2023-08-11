96bd545885ddf7f2e48c4734e55f3da3576be6d6
nismal <nismalm14@gmail.com>
2023-08-11 11:51:51 +0530

    data changed

--- sellover/settings.py
+++ sellover/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'sellover.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,11 +121,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
-STATICFILES_DIRS =os.path.join(BASE_DIR, 'static'),
-MEDIA_ROOT = BASE_DIR / 'static/media'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR , "static")
+}
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
