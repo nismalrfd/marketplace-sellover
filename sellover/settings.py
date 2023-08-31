1853b537b52e496dcde213daac6e368056372e40
nismal <nismalm14@gmail.com>
2023-08-31 16:56:21 +0530

    updated

--- sellover/settings.py
+++ sellover/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gwa49w&nb42fyui3$rk#gx6!)73979ux)(9kzljptp$&umdw6g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
